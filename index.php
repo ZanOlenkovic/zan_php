ad49b00d14efc9d9d5ad664dced40d93baeb571f
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-29 08:13:39 -0700

    String junginiai 2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   </head>
   <body>
     <header>
-            <h1> <b> Zan </b> - PHP su manimi buvo <?php print date('h' , strtotime('-1 hour')). 'valanda!'; ?></h1>
+            <h1> <b> Zan </b> - PHP su manimi buvo <?php print date('h' , strtotime('-1 hour')). ' valanda!'; ?></h1>
             <p>  <?php print date('Y' , strtotime('+1 year')); ?> ne uz kalnu! </p>
     </header>
   </body>
