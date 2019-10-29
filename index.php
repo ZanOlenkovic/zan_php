ab518a3b7c11cd747960ff0aae1e84b4bfe4fce0
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-29 07:47:16 -0700

    String junginiai

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
     <header>
             <h1> <b> Zan </b> - HTML ir <?php print'ir PHP' ?> asas jau nuo <?php print date('o') ?></h1>
-            <p> Viskas prasidejo <?php print date('F') ?> menesio, <?php print date('d') ?> </p>
+            <p> Viskas prasidejo <?php print date('F') ?> menesio, <?php print date('d') ?> diena! </p>
     </header>
   </body>
 </html>
