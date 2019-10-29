db50220488055df9414fa1a9f101a317a16623de
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-29 17:32:30 +0200

    String junginiai 3

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
-  <head>
-   
-      <title> Komentarai</title>
-  </head>
-  <body>
-            <h1> Dainu Tekstai </h1>
-            <h2> Paragrafo Daina </h2>
-            <p> As tok vienisas jaunas paragrafas... </p>
-            <p> Mindau svetimo kodo gatves...</p>
-  </body>
+    <head>
+        <title> Komentarai</title>
+    </head>
+    <body>
+        <h1> Dainu Tekstai </h1>
+        <h2> Paragrafo Daina </h2>
+        <p> As tok vienisas jaunas paragrafas... </p>
+        <!--<p> Mindau svetimo kodo gatves...</p> -->
+        <p
+    </body>
 </html>
