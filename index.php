e950604b556aab385b4fbf3c9acddfd407b238f5
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-29 08:12:22 -0700

    String junginiai 2

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
   <head>
    
-      <title> <?php print 'As ' . date('l') . ' ir PHP' ?> </title>
+      <title> PHP lydes ir <?php print date('Y-m-d' , strtotime('+1 day')); ?> </title>
   </head>
   <body>
     <header>
-            <h1> <b> Zan </b> - HTML ir <?php print'ir PHP' ?> asas jau nuo <?php print date('o') ?></h1>
-            <p> Viskas prasidejo <?php print date('F') ?> menesio, <?php print date('d') ?> diena! </p>
+            <h1> <b> Zan </b> - PHP su manimi buvo <?php print date('h' , strtotime('-1 hour')). 'valanda!'; ?></h1>
+            <p>  <?php print date('Y' , strtotime('+1 year')); ?> ne uz kalnu! </p>
     </header>
   </body>
 </html>
