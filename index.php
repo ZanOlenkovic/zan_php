91d5b9d6cbd54bb2c27c69ac1d4b6f9ef1ec7ad9
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-30 16:34:34 +0200

    bomba

--- index.php
+++ index.php
@@ -2,39 +2,21 @@
     <head>
         <title> PHP lydes ir <?php print date('Y', strtotime('+' . rand(0, 10) . 'year')) ?></title>
         <style>
-            .dice{
-                width: 250px;
-                height: 250px;
+            body {
+                display: flex;
+                flex-direction: column;
+                align-item: center;
             }
-            
-            .k1{
-                background-image: url("http://dobbelsteen.virtuworld.net/img/1c.gif") ;
-            }
-            .k2{
-                background-image: url("http://dobbelsteen.virtuworld.net/img/2c.gif") ;
-            }
-            .k3{
-                background-image: url("http://dobbelsteen.virtuworld.net/img/3c.gif") ;
-            }
-            .k4{
-                background-image: url("http://dobbelsteen.virtuworld.net/img/4c.gif") ;
-            }
-            .k5{
-                background-image: url("http://dobbelsteen.virtuworld.net/img/5c.gif") ;
-            }
-            .k6{
-                background-image: url("http://dobbelsteen.virtuworld.net/img/6c.gif") ;
-            }
-          
+            .b{
+                width: <?php print date('s'); ?>px;
+                height: <?php print date('s'); ?>px;
+                background-size: cover;
+                background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAR_iJB0tLQMLXGJi6kn1AOjvAWkrGiLUnmfSRYAt07qWSQvXi-w&s") ;
+            } 
         </style>
     </head>
     <body>
-
-          <div class="dice k<?php print rand(1, 6); ?> "></div>
-
-
-
-       
-
+        <div class="b"></div>
+        <p> <?php print date('s') ?></p>
     </body>
 </html>
