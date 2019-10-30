3d38e0c82b91617ec6965b80c682498cfb7b73a7
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-30 15:38:05 +0200

    kauliukas

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <html>
     <head>
-        <title> PHP lydes ir <?php print date('Y' , strtotime('+'. rand(0,10) . 'year') ) ?></title>
+        <title> PHP lydes ir <?php print date('Y', strtotime('+' . rand(0, 10) . 'year')) ?></title>
+        <style>
+            .dice{
+                width: 250px;
+                height: 250px;
+            }
+            
+            .k1{
+                background-image: url("http://dobbelsteen.virtuworld.net/img/1c.gif") ;
+            }
+            .k2{
+                background-image: url("http://dobbelsteen.virtuworld.net/img/2c.gif") ;
+            }
+            .k3{
+                background-image: url("http://dobbelsteen.virtuworld.net/img/3c.gif") ;
+            }
+            .k4{
+                background-image: url("http://dobbelsteen.virtuworld.net/img/4c.gif") ;
+            }
+            .k5{
+                background-image: url("http://dobbelsteen.virtuworld.net/img/5c.gif") ;
+            }
+            .k6{
+                background-image: url("http://dobbelsteen.virtuworld.net/img/6c.gif") ;
+            }
+          
+        </style>
     </head>
     <body>
-        <h1> <b>Zan</b> - Galbut turesiu <?php print rand(1,5) ?> vaiku(us)! </h1>
-        <h2> Paragrafo Daina </h2>
-         <p> D. Trump'as nebeprezidentu: <?php print date('Y' , strtotime('+' . rand(2,10) . 'year')). date(' m-d') ?></p>
-      
+
+          <div class="dice k<?php print rand(1, 6); ?> "></div>
+
+
+
+       
 
     </body>
 </html>
