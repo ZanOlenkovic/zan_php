51ec9d5320e3d6ecee6ebdcfead791d60e0d8492
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-30 13:59:33 +0200

    Naujas

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <h1> <b>Zan</b> - Galbut turesiu <?php print rand(1,5) ?> vaiku(us)! </h1>
         <h2> Paragrafo Daina </h2>
-         <p> D. Trump'as nebebus prezidentu: <?php print date('Y' , strtotime('+' . rand(2,10) . 'year')). date(' m-d') ?></p>
+         <p> D. Trump'as nebeprezidentu: <?php print date('Y' , strtotime('+' . rand(2,10) . 'year')). date(' m-d') ?></p>
       
 
     </body>
