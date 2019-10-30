6f4b94e67a2d8d0e7a599469afb804a4a8d86fcf
ZanOlenkovic <zanolenkovic@gmail.com>
2019-10-30 17:12:16 +0200

    bomba

--- index.php
+++ index.php
@@ -5,18 +5,24 @@
             body {
                 display: flex;
                 flex-direction: column;
-                align-item: center;
+                align-items: center;
             }
-            .b{
-                width: <?php print date('s'); ?>px;
-                height: <?php print date('s'); ?>px;
+            .b-<?php print date('s')?> {
+                width: <?php print date('1s'); ?>vh;
+                height: <?php print date('1s'); ?>vh;
                 background-size: cover;
                 background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAR_iJB0tLQMLXGJi6kn1AOjvAWkrGiLUnmfSRYAt07qWSQvXi-w&s") ;
             } 
-        </style>
+            .b-00 {
+                height: 60vh;
+                width: 60vh;
+                background-image: url(https://www.ready.gov/sites/default/files/2019-09/hero_nuclear_blast.jpg);
+            }
+          </style>
     </head>
     <body>
-        <div class="b"></div>
-        <p> <?php print date('s') ?></p>
+        <div class="b-<?php print date('s');?>"></div>
+        
+        <p> <?php print date('s'); ?></p>
     </body>
 </html>
