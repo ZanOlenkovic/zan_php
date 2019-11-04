dd8d6452520cbcedd78ef85a4867fb903b509e54
ZanOlenkovic <zanolenkovic@gmail.com>
2019-11-04 17:45:39 +0200

    bbuxlo

--- index.php
+++ index.php
@@ -1,37 +1,33 @@
 <?php
-$bool = true;
-$int = 1;
-if ($bool === $int) {
-    $li_1 = 'Identiski';
-} elseif ($bool == $int) {
-    $li_1 = 'Lygus';
+$sekunde = date('s');
+if ($sekunde % 2) {
+    $class = 'apskritimas';
+} else {
+    $class = 'kvadratas';
 }
-$str = '1';
-$bool = true;
-if ($str === $bool) {
-    $li_2 = 'Identiski';
-} elseif ($str == $bool) {
-    $li_2 = 'Lygus';
-}
-$flt = 1.23;
-$str = '1.23';
-if ($flt === $str) {
-    $li_3 = 'Identiski';
-} elseif ($flt == $str) {
-    $li_3 = 'Lygus';
-}
-$ly_1 = "Bool ir Integer: $li_1";
-$ly_2 = "Bool ir Integer: $li_2";
-$ly_3 = "Bool ir Integer: $li_3";
 ?>
 <html> 
     <head>
         <title><?php print 'orai' ?></title>
+        <style>
+            div{
+                display:flexbox;
+                justify-content: center;
+                background-color: purple;
+                width: 200px;
+                height: 200px;
+
+            }
+
+            .apskritimas {
+                border-radius: 100px;
+                background-color: red;
+            }
+        </style>
     </head>
     <body>
-        <p><?php print $ly_1; ?></p>
-        <p><?php print $ly_2; ?></p>
-        <p><?php print $ly_3; ?></p>
+        <div class="<?php print $class; ?>">
+        </div>
     </body>
 
 </html>
