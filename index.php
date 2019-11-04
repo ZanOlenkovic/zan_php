b05163a03ebdc6a26d48fc9c7ee8c42edcd2d4c0
ZanOlenkovic <zanolenkovic@gmail.com>
2019-11-04 15:09:17 +0200

    bbuxlo

--- index.php
+++ index.php
@@ -3,22 +3,19 @@
         <title> PHP lydes ir <?php print date('Y', strtotime('+' . rand(0, 10) . 'year')) ?></title>    
     </head>
     <body>
-      <?php
-      $grizai_velai = rand(0,1);
-      $grizai_isgeres = rand(0,1);
-      if($grizai_velai && $grizai_isgeres){
-          $h2='Grizai velai ir isgeres';
-      }
-      elseif($grizai_velai && !$grizai_isgeres){
-          $h2='Grizai velai';
-      }
-       elseif($grizai_isgeres && !$grizai_velai){
-          $h2='Grizai isgeres';
-      }
-      else{
-          $h2='Nieko nepadarei';
-      }
-      ?>
+        <?php
+        $grizai_velai = rand(0, 1);
+        $grizai_isgeres = rand(0, 1);
+        if ($grizai_velai && $grizai_isgeres) {
+            $h2 = 'Grizai velai ir isgeres';
+        } elseif ($grizai_velai && !$grizai_isgeres) {
+            $h2 = 'Grizai velai';
+        } elseif ($grizai_isgeres && !$grizai_velai) {
+            $h2 = 'Grizai isgeres';
+        } else {
+            $h2 = 'Nieko nepadarei';
+        }
+        ?>
         <h1>Buitine skaiciuokle</h1>
         <h2><?php print $h2 ?></h2>
     </body>
